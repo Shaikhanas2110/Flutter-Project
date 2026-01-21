0aff3930a36c8cebbef7a0b76544565d9d2cfc19
Anas Shaikh <shaikhanas2354@gmail.com>
2026-01-21 20:46:10 +0530

    Unnecessary code removed

--- lib/views/data/classes/notification_service.dart
+++ lib/views/data/classes/notification_service.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
-import 'package:timezone/timezone.dart' as tz;
-import 'package:flutter_timezone/flutter_timezone.dart';
 
 class NotificationService {
   static final FlutterLocalNotificationsPlugin _notifications =
